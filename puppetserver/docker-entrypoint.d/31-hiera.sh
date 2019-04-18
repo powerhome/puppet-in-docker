aa8537fe49447c2206ebefb9b6f6b82994fac760
Bernardo Donadio <bcdonadio@bcdonadio.com>
2019-04-18 00:09:14 -0300

    Import custom GPG key to Hiera

--- puppetserver/docker-entrypoint.d/31-hiera.sh
+++ puppetserver/docker-entrypoint.d/31-hiera.sh
@@ -4,3 +4,9 @@ if [ -n "$HIERA_BASE64" ]; then
   echo "---> Saving Hiera configuration to /etc/puppetlabs/puppet/hiera.yaml (base64 decoded)"
   echo -e "$HIERA_BASE64" | base64 -d > /etc/puppetlabs/puppet/hiera.yaml
 fi
+
+if [ -n "$HIERA_GPG_KEY_BASE64" ]; then
+  echo "---> Importing GPG key to Hiera Keyring"
+  mkdir ${GNUPGHOME}
+  echo "$HIERA_GPG_KEY_BASE64" | base64 -d | gpg --import
+fi
